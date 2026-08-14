485b9fce7915da6dc953765a2e326681800d27ce
Aksha <aksha@example.com>
2026-08-14 21:06:13 +0530

    new

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "jasNext",
+  description: "jashpur walo ke liye",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="container">
+      <header className="bg-gray-800 text-white p-4">Header</header>
+    </div>
+  );
+};
+
+export default Header;
