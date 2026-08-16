237c810272154a5f478d5afc2885575867c7e43a
Aksha <aksha@example.com>
2026-08-16 19:59:27 +0530

    new

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaAnglesDown } from "react-icons/fa6";
 
 const Nav = () => {
   return (
@@ -13,12 +14,29 @@ const Nav = () => {
             <Link href= '/' className='text-[17px] text-gray-800 font-[600] hover:text-primary'>Construction</Link>
             <Link href= '/' className='text-[17px] text-gray-800 font-[600] hover:text-primary'>Messages</Link>
             <Link href= '/' className='text-[17px] text-gray-800 font-[600] hover:text-primary'>News</Link>
+        
+        <div className='relative group'>
             
+            <span className='text-[15px] text-gray-800 font-[600] hover:text-primary flex items-center gap-1
+            cursor-pointer'>More <FaAnglesDown size={18}/> </span>
+
+         <div className='dropdown-menu flex flex-col absolute top-full right-2px bg-orange-100 shadow-md rounded-md
+        overflow-hidden w-[200px] invisible opacity-0 transition-all duration-200 group-hover:opacity-100 group-hover:visible pt-4'>
+           
+
+            <Link href= '/' className='text-[16px] text-gray-800 font-[600] hover:text-primary'>jp-olx</Link>
+             <Link href= '/' className='text-[16px] text-gray-800 font-[600] hover:text-primary'>Beverages</Link>
+              <Link href= '/' className='text-[16px] text-gray-800 font-[600] hover:text-primary'>Beverages</Link>
+               <Link href= '/' className='text-[16px] text-gray-800 font-[600] hover:text-primary'>Beverages</Link>
+                <Link href= '/' className='text-[16px] text-gray-800 font-[600] hover:text-primary'>Beverages</Link>
+
 
         </div>
+        </div>
+          </div>
       
     </nav>
   );
-};
+    };
 
 export default Nav;
