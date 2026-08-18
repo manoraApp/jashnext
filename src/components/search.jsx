29ad3012723bdb632a71ff46ab249ecec8f42a48
Aksha <aksha@example.com>
2026-08-18 20:12:24 +0530

    up

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,26 +1,25 @@
-
 import React from "react";
 import { IoSearchOutline } from "react-icons/io5";
 
-
-
-
-
 const Search = () => {
   return (
-    <div className="search bg-[#E6E6E6] w-[600px] h-[50px] rounded-md px-4 relative border-[rgba(0,0,0,0.1)] 
-    hover:border-[rgba(0,0,0,0.3)]" >
-
-      <input type="text" className="w-full h-full  outline-none border-0 placeholder:text-gray-500" placeholder="Search for product..." />
-       <button    className='w-10 h-10 rounded-full bg-gray-00 absolute top-[5px] right-2 z-50 flex items-center 
-       justify-center cursor-pointer hover:bg-gray-200 transition-colors duration-300'> 
-        <IoSearchOutline size={25} /> 
-        </button>
-     
+    <div
+      className="search bg-[#E6E6E6] w-[600px] h-[50px] rounded-md px-4 relative border-[rgba(0,0,0,0.1)] 
+    hover:border-[rgba(0,0,0,0.3)]"
+    >
+      <input
+        type="text"
+        className="w-full h-full  outline-none border-0 placeholder:text-gray-500"
+        placeholder="Search for product..."
+      />
+      <button
+        className="w-10 h-10 rounded-full bg-gray-00 absolute top-[5px] right-2 z-50 flex items-center 
+       justify-center cursor-pointer hover:bg-gray-200 transition-colors duration-300"
+      >
+        <IoSearchOutline size={25} />
+      </button>
     </div>
   );
 };
 
 export default Search;
-
-
