f905631cd517412d9fd7b2f4ad80fa924ef4d8b5
manoraApp <manora.app@gmail.com>
2026-08-20 22:15:56 +0530

    jkhjj

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 
+import Link from "next/link";
+
 const ProductItem = () => {
   return (
-    <div className="productItem shadow-md w-full bg-white rounded-md "> 
-    <div className='img overflow-hidden'>
-        <img src={'/cat9.png'} alt= 'product image'/>
+    <div className="productItem shadow-md w-full bg-white rounded-md ">
+      <Link href={"/"} className='img overflow-hidden group flex p-3'>
+        <img src={'/R1.png'} alt='product image' className='transition group-hover:scale-105' />
+      </Link>
+      <div className='info p-3'>
+
+        <Link href={"/"} className="text-[15px] text-grey-800 font-[500]">100 percent apple juice-64 f1 oz Bottle</Link>
+
+      </div>
     </div>
-     
-            
-    
+  )
 
-
-
-
-
-
-
-    
-      
-    </div>
-  );
 };
+
+
 
 export default ProductItem;
