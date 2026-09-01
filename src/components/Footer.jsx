52f3f77a642fecd112c7d6a23251ba190f5d6fc7
manoraApp <manora.app@gmail.com>
2026-09-01 18:09:50 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import { PiKeyReturnLight } from "react-icons/pi";
+
+const Footer = () => {
+  return (
+    <footer className='bg-[#FAFAFA] py-8' >
+
+
+      <div className='container '>
+<div className='col flex items-center justify-center flex-col group w-[15%]'>
+  <PiKeyReturnLight className='text-[40px] font-[600] mt-3'/>
+    <h3 className='text-[16px] font-[600] mt-3'>30 Days Returns </h3>
+    <p className='text-[12px] font-[500] mt-3' >For an Exchange Product </p>
+</div>
+
+
+
+ <div className='container '>
+<div className='col flex items-center justify-center flex-col group w-[15%]'>
+  <PiKeyReturnLight className='text-[40px] font-[600] mt-3'/>
+    <h3 className='text-[16px] font-[600] mt-3'>30 Days Returns </h3>
+    <p className='text-[12px] font-[500] mt-3' >For an Exchange Product </p>
+</div>
+
+
+
+ <div className='container '>
+<div className='col flex items-center justify-center flex-col group w-[15%]'>
+  <PiKeyReturnLight className='text-[40px] font-[600] mt-3'/>
+    <h3 className='text-[16px] font-[600] mt-3'>30 Days Returns </h3>
+    <p className='text-[12px] font-[500] mt-3' >For an Exchange Product </p>
+</div>
+
+</div>
+</div>
+</div>
+
+
+    </footer>
+    
+      
+   
+  );
+};
+
+export default Footer;
